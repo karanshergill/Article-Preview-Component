40e13e081bf0f2f6d4780f2965b6cf4361239389
Karan <email@karanshergill.com>
2024-11-01 21:29:33 +0530

    fix mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          'very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+          'desaturated-dark-blue': 'hsl(214, 17%, 51%)',
+          'grayish-blue': 'hsl(212, 23%, 69%)',
+          'light-grayish-blue': 'hsl(210, 46%, 95%)'
+        },
+      },
+      fontFamily: {
+        'manrope': '"Manrope", "sans-serif";'
+      },
+    },
   },
   plugins: [],
 }
